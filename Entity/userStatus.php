7175134b377cace6aa2512576c343f13ad003fdb
manu7772 <manu7772@gmail.com>
2014-09-29 16:26:08 +0200

    adaptation vendor bundle Labo 24

---
+++ Entity/userStatus.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace labo\Bundle\TestmanuBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Doctrine\Common\Collections\ArrayCollection;
+// Slug
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\MappedSuperclass
+ * @UniqueEntity(fields={"nom"}, message="Ce statut existe déjà")
+ */
+abstract class userStatus {
+
+	private $id;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="nom", type="string", length=100, nullable=false, unique=true)
+	 * @Assert\NotBlank(message = "Vous devez remplir ce champ.")
+	 * @Assert\Length(
+	 *      min = "3",
+	 *      max = "100",
+	 *      minMessage = "Le nom doit comporter au moins {{ limit }} lettres.",
+	 *      maxMessage = "Le nom doit comporter au maximum {{ limit }} lettres."
+	 * )
+	 */
+	private $nom;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="descriptif", type="text", nullable=true)
+	 */
+	private $descriptif;
+
+
+	/**
+	 * Get id
+	 *
+	 * @return integer 
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * Set nom
+	 *
+	 * @param string $nom
+	 * @return userStatus
+	 */
+	public function setNom($nom) {
+		$this->nom = $nom;
+	
+		return $this;
+	}
+
+	/**
+	 * Get nom
+	 *
+	 * @return string 
+	 */
+	public function getNom() {
+		return $this->nom;
+	}
+
+	/**
+	 * Set descriptif
+	 *
+	 * @param string $descriptif
+	 * @return userStatus
+	 */
+	public function setDescriptif($descriptif) {
+		$this->descriptif = $descriptif;
+	
+		return $this;
+	}
+
+	/**
+	 * Get descriptif
+	 *
+	 * @return string 
+	 */
+	public function getDescriptif() {
+		return $this->descriptif;
+	}
+}
