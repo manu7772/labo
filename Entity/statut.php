8c45c6be8f144c442700e1748725515534a0da86
manu7772 <manu7772@gmail.com>
2014-09-29 02:39:08 +0200

    adaptation vendor bundle Labo 7

--- Entity/statut.php
+++ Entity/statut.php
@@ -6,8 +6,11 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Doctrine\Common\Collections\ArrayCollection;
+// Slug
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
+ * @ORM\MappedSuperclass
  * @UniqueEntity(fields={"nom"}, message="Ce statut existe déjà.")
  */
 abstract class statut {
