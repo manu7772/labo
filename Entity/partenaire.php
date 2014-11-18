c40bd9f45f6bb35abc0c95af9c9e1bb4f2119f9f
manu7772 <manu7772@gmail.com>
2014-11-18 17:30:14 +0100

    suppression typePartenaire

--- Entity/partenaire.php
+++ Entity/partenaire.php
@@ -154,11 +154,6 @@ abstract class partenaire {
      */
     protected $statut;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="AcmeGroup\LaboBundle\Entity\typePartenaire")
-     * @ORM\JoinColumn(nullable=true, unique=false)
-     */
-    protected $typePartenaire;
 
 
     public function __construct() {
@@ -613,29 +608,6 @@ abstract class partenaire {
      */
     public function getStatut() {
         return $this->statut;
-    }
-
-    /**
-     * Set typePartenaire
-     *
-     * @param \AcmeGroup\LaboBundle\Entity\typePartenaire $typePartenaire
-     * @return partenaire
-     */
-    public function setTypePartenaire(\AcmeGroup\LaboBundle\Entity\typePartenaire $typePartenaire)
-    {
-        $this->typePartenaire = $typePartenaire;
-    
-        return $this;
-    }
-
-    /**
-     * Get typePartenaire
-     *
-     * @return \AcmeGroup\LaboBundle\Entity\typePartenaire 
-     */
-    public function getTypePartenaire()
-    {
-        return $this->typePartenaire;
     }
 
 
