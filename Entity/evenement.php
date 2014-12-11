29b084e7a63656e99a6a6c7339a201879769c064
manu7772 <manu7772@gmail.com>
2014-12-11 16:06:16 +0100

    Test HasLifecycleCallbacks sur Evenement

--- Entity/evenement.php
+++ Entity/evenement.php
@@ -11,6 +11,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\MappedSuperclass
+ * @ORM\HasLifecycleCallbacks()
  */
 abstract class evenement {
 	
@@ -373,6 +374,13 @@ abstract class evenement {
 	 */
 	public function getDateCreation() {
 		return $this->dateCreation;
+	}
+
+	/**
+	 * @ORM\PreUpdate
+	 */
+	public function updateDateMaj() {
+		$this->setDateMaj(new \Datetime());
 	}
 
 	/**
