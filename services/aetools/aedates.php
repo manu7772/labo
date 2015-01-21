2c64529d387f91816e71502af07a855aa6f7fd5a
manu7772 <manu7772@gmail.com>
2015-01-21 22:54:27 +0100

    Service adeptes / compléments

--- services/aetools/aedates.php
+++ services/aetools/aedates.php
@@ -14,5 +14,27 @@ class aedates {
 		return $this;
 	}
 
+	private function getCalendEnCours() {
+		$date = new \Datetime();
+		$data = array();
+		$data['date']['jourDeLannee'] = intval($date->format("z"));
+		$data['date']['jourDeSemaine'] = intval($date->format("w"));
+		$data['date']['semaineEnCours'] = intval($date->format("W"));
+		$data['date']['moisEnCours'] = intval($date->format("n"));
+		$data['date']['anneeEnCours'] = intval($date->format("Y"));
+		return $data;
+	}
 
+	private function isDateValid($date, $tempo = "mois", $ecart = 12) {
+		$tempos = array("jour", "semaine", "mois", "annee");
+		if(!in_array(strtoupper($tempo), $tempos)) $tempo = $tempo[2];
+		// date actuelle
+		$date = new \Datetime();
+		// date $date
+		$Fmois = intval($date->format("n"));
+		$Fannee = intval($date->format("Y"));
+		return true;
+	}
+
+}
 ?>
