d7116c36f10dacaf13d8bb4cc2e895c1e9c53bee
manu7772 <manu7772@gmail.com>
2015-03-07 10:54:20 +0100

    mise à jour aeReponse

--- services/aetools/aeReponse.php
+++ services/aetools/aeReponse.php
@@ -5,12 +5,31 @@ namespace labo\Bundle\TestmanuBundle\services\aetools;
 
 class aeReponse {
 
-	private $data = array();
+	const NOM_MESSAGES = 'info';
+	const NOM_ERRORMESSAGES = 'error';
 
-	public function __construct($result, $data = null, $message = "") {
-		$this->data["result"] 	= $result;
-		$this->data["message"] 	= $message;
-		$this->data["data"] 	= $data;
+	protected $container;		// container
+	protected $flashBag;		// session
+	protected $data = array();	// data
+
+	public function __construct(ContainerInterface $container) {
+		$this->container 	= $container;
+		$this->flashBag 	= $this->container->get('request')->getSession()->getFlashBag();
+		// if(!is_array($data)) $data = array($data);
+		// if(!is_array($messages)) $messages = array($messages);
+		// if(!is_array($ERRORmessages)) $ERRORmessages = array($ERRORmessages);
+		$this->data["data"] = array();
+		$this->data["messages"] = array();
+		$this->data["ERRORmessages"] = array();
+
+		$this->setResult(true);
+		// $this->setMessage($messages);
+		// $this->setErrorMessage($ERRORmessages);
+		// $this->setResult($result);
+	}
+
+	protected function computeData() {
+		if($this->hasErrors() === true) $this->setUnvalid();
 	}
 
 	// GETTERS
@@ -19,40 +38,132 @@ class aeReponse {
 		return $this->data["result"];
 	}
 
-	public function getMessage() {
-		return $this->data["message"];
+	public function getMessages() {
+		return $this->data["messages"];
 	}
 
-	public function getData() {
-		return $this->data["data"];
+	public function getErrorMessages() {
+		return $this->data["ERRORmessages"];
 	}
 
-	public function getDataType() {
-		return gettype($this->data["data"]);
+	public function getAllMessages($mix = false) {
+		if($mix === false) return array("messages" => $this->data["messages"], "ERRORmessages" => $this->data["ERRORmessages"]);
+		return array_merge($this->data["messages"], $this->data["ERRORmessages"]);
+	}
+
+	public function getData($nom = null, $efface = false) {
+		if(is_string($nom) && isset($this->data["data"][$nom])) {
+			$result = $this->data["data"][$nom];
+			if($efface === true) unset($this->data["data"][$nom]);
+		} else {
+			$result = $this->data["data"];
+			if($efface === true) {
+				unset($this->data["data"]);
+				$this->data["data"] = array();
+			}
+		}
+		return $result;
+	}
+
+	public function getDataAndSupp($nom = null) {
+		return $this->getData($nom, true);
+	}
+
+	public function getDataType($nom) {
+		if(isset($this->data["data"][$nom])) return gettype($this->data["data"][$nom]);
+			else return false;
+	}
+
+	public function getDataKeys() {
+		return array_keys($this->data["data"]);
+	}
+
+	public function isDataKey($key) {
+		return (array_key_exists($key, array_keys($this->data["data"])) ? true : false );
 	}
 
 	public function getJSONreponse() {
 		return json_encode($this->data);
 	}
 
+	public function isValid() {
+		return ($this->data["result"] === true ? true : false );
+	}
+
+	public function hasData() {
+		return (count($this->data["data"]) > 0 ? true : false );
+	}
+
+	public function hasMessages() {
+		return (count($this->data["messages"]) > 0 ? true : false );
+	}
+
+	public function hasErrors() {
+		return (count($this->data["ERRORmessages"]) > 0 ? true : false );
+	}
+
 	// SETTERS
 
-	public function setResult($result) {
+	public function setValid() {
+		$this->setResult(true);
+		return $this;
+	}
+
+	public function setUnvalid() {
+		$this->setResult(false);
+		return $this;
+	}
+
+	public function setResult($result = true) {
 		if(!is_bool($result)) $result = false;
 		$this->data["result"] = $result;
 		return $this;
 	}
 
-	public function setMessage($message = null) {
-		$this->data["message"] = $message;
+	public function addMessage($messages) {
+		if(!is_array($messages)) $messages = array($messages);
+		foreach ($messages as $message) {
+			$this->data["messages"][] = $message;
+		}
 		return $this;
 	}
 
-	public function setData($data = null) {
-		$this->data["data"] = $data;
+	public function addErrorMessage($ERRORmessages, $unvalidate = true) {
+		if(!is_array($ERRORmessages)) $ERRORmessages = array($ERRORmessages);
+		foreach ($ERRORmessages as $ERRORmessage) {
+			$this->data["ERRORmessages"][] = $ERRORmessage;
+		}
+		if($unvalidate === true) $this->computeData();
 		return $this;
 	}
 
+	public function addData($data, $nom = null) {
+		if($nom === null) $this->data["data"][] = $data;
+			else $this->data["data"][$nom] = $data;
+		return $this;
+	}
+
+	// AUTRES
+
+	public function putAllMessagesInFlashbag() {
+		$this->putMessagesInFlashbag();
+		$this->putErrorMessagesInFlashbag();
+		return $this;
+	}
+
+	public function putErrorMessagesInFlashbag() {
+		foreach ($this->data['ERRORmessages'] as $value) {
+			$this->flashBag->add(self::NOM_ERRORMESSAGES, $value);
+		}
+		return $this;
+	}
+
+	public function putMessagesInFlashbag() {
+		foreach ($this->data['messages'] as $value) {
+			$this->flashBag->add(self::NOM_MESSAGES, $value);
+		}
+		return $this;
+	}
 
 
 }
